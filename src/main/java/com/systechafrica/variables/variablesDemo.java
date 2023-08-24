91ac69efadc02ab4a8f7f61c98801fa8ebcb4dee
LenaBett <rheabett@gmail.com>
2023-08-24 20:12:46 +0300

    Working with variables

--- src/main/java/com/systechafrica/variables/variablesDemo.java
+++ src/main/java/com/systechafrica/variables/variablesDemo.java
@@ -24,6 +24,33 @@ public class variablesDemo {
 
         LOGGER.info("Maximumm Long value is " + Long.MAX_VALUE);
         LOGGER.info("Minimumm Long value is " + Long.MIN_VALUE);
+        // byte, short, long, int
+        //? Wrapper classes
+
+        byte aPrimitive = 15;
+        Byte aWrapper = 15;
+        
+        LOGGER.info("byte primitive : " + aPrimitive);
+        LOGGER.info("Byte Wrapper: " + aWrapper);
+
+        int a = aWrapper.intValue();
+
+        LOGGER.info("Integer value from Byte Wrapper: " + a);
+
+        // Short
+        short s = 23;
+        Short aShort = 23;
+
+        // Integer
+        int c = 23;
+        Integer aInt = 23;
+
+        // Long
+        long d = 23;
+
+        //! you need to explicitly suffic the long wrapper with an 'L'
+        Long aLong = 23L;
+
     }
 
     private void floatingTypes(){
@@ -32,12 +59,23 @@ public class variablesDemo {
     double doublerate2 = 3.147569854; 
     LOGGER.info("Float value is " + floatrate);
     LOGGER.info("Float value is " + doublerate2);
+
+    // float
+    float price = 0.2365F;
+    Float priceFloat = 0.2365F;
+    //double
+    double prtPrice = 0.2365;
+    Double priceDouble = 0.2365;
+
     }
 
     private void characterTypes(){
        // character type
        char grade = 'A';
        LOGGER.info("The student's grade is : " + grade);
+        //character type
+        Character characterGrade = 'G';
+
     }
 
      private void booleanTypes(){
@@ -58,6 +96,7 @@ public class variablesDemo {
         demo.floatingTypes();
         demo.characterTypes();
         demo.booleanTypes();
+    
     }      
 }
     
