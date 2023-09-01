d04ee5477f67c07c5763b899be43e8f3f4469140
LenaBett <rheabett@gmail.com>
2023-09-01 15:25:35 +0300

    Operators

--- src/main/java/com/systechafrica/Operators/operatorsDemo.java
+++ src/main/java/com/systechafrica/Operators/operatorsDemo.java
@@ -3,10 +3,13 @@ package com.systechafrica.Operators;
 import java.util.logging.Logger;
 
 public class operatorsDemo {
+    // Operators are symbols that perform operations on operands.
+    // Operands can be Variables, Constants or expressions
+
     private static final Logger LOGGER = Logger.getLogger(operatorsDemo.class.getName());
 
     public void arithmeticOperators() {
-
+        // Assignment Operator
         int studentOneAge = 23;
         int studentTwoAge = 19;
 
@@ -31,7 +34,7 @@ public class operatorsDemo {
         double divisionResultInDouble = (double) studentOneAge / studentTwoAge;
         LOGGER.info("Division with precision: " + divisionResultInDouble);
 
-        // Modulus operator => remainder of division result
+        // Modulus operator => give the remainder of division result
         int remainder = studentOneAge % studentTwoAge;
         LOGGER.info("Remainder: " + remainder);
 
@@ -42,14 +45,15 @@ public class operatorsDemo {
         int quantity = 35;
 
         // addition assignment operator
-        quantity += 10; // > quantity = quantity + 10;
+        quantity += 10; // quantity = quantity + 10;
         LOGGER.info("Quantity: " + quantity);
 
         // multiplication assignment operator
-        quantity *= 100; // > quantity = quantity * 100;
+        quantity *= 100; // quantity = quantity * 100;
         LOGGER.info("Quantity: " + quantity);
-        // multiplication assignment operator
-        quantity /= 20; // > quantity = quantity / 20;
+
+        // division assignment operator
+        quantity /= 20; // quantity = quantity / 20;
         LOGGER.info("Quantity: " + quantity);
     }
 
@@ -57,19 +61,19 @@ public class operatorsDemo {
         int studentOneAge = 20;
         int studentTwoAge = 25;
 
-        // equality operator
+        // Equality operator
         boolean isStudentOneAgeEqualToStudentTwoAge = studentOneAge == studentTwoAge;
         LOGGER.info("isStudentOneAgeEqualToStudentTwoAge: " + isStudentOneAgeEqualToStudentTwoAge);
 
-        // Inequality operator
+        // Inequality (Not Equal) operator
         boolean isStudentOneAgeNotEqualToStudentTwoAge = studentOneAge != studentTwoAge;
         LOGGER.info("isStudentOneAgeNotEqualToStudentTwoAge: " + isStudentOneAgeNotEqualToStudentTwoAge);
 
-        // less than operator
+        // less-than operator
         boolean isStudentOneAgeLessThanStudentTwoAge = studentOneAge < studentTwoAge;
         LOGGER.info("isStudentOneAgeLessThanStudentTwoAge: " + isStudentOneAgeLessThanStudentTwoAge);
 
-        // greater than operator
+        // greater-than operator
         boolean isStudentOneAgeGreaterThanStudentTwoAge = studentOneAge > studentTwoAge;
         LOGGER.info("isStudentOneAgeGreaterThanStudentTwoAge: " + isStudentOneAgeGreaterThanStudentTwoAge);
 
@@ -82,23 +86,23 @@ public class operatorsDemo {
         LOGGER.info("IsStudentOneAgeGreaterThanOrEqualToStudentTwoAge: " + isStudentOneAgeGreaterThanOrEqualToStudentTwoAge);
     }
 
-    public void andLogicalOperator(){
+    public void andLogicalOperator() {
         int studentOneAge = 17;
         int studentTwoAge = 25;
 
         boolean isStudentOneAllowed = studentOneAge >= 18;
-        boolean isStudentTwoAllowed =studentTwoAge >= 18;
+        boolean isStudentTwoAllowed = studentTwoAge >= 18;
 
         // AND operator
-        if(isStudentOneAllowed && isStudentTwoAllowed){
+        if (isStudentOneAllowed && isStudentTwoAllowed) {
             LOGGER.info("Students allowed for ID registration");
-            }else{
-                LOGGER.info("Not allowed for registration");
-            }
+        } else {
+            LOGGER.info("Not allowed for registration");
+        }
 
     }
 
- public void orLogicalOperator(){
+    public void orLogicalOperator() {
         int studentOneAge = 17;
         int studentTwoAge = 25;
 
@@ -110,9 +114,9 @@ public class operatorsDemo {
         } else {
             LOGGER.info("Not allowed  for ID registration");
         }
-     }
+    }
 
- public void notLogicalOperator(){
+    public void notLogicalOperator() {
         int studentOneAge = 17;
         boolean isStudentOneAllowed = studentOneAge >= 18;
         // and operator
@@ -128,7 +132,7 @@ public class operatorsDemo {
         operatorsDemo app = new operatorsDemo();
         app.arithmeticOperators();
         app.assignmentOperators();
-        app.comparisonOperators(); 
+        app.comparisonOperators();
         app.andLogicalOperator();
         app.orLogicalOperator();
         app.notLogicalOperator();
