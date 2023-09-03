96f68ed91264b2749cbdaefb1febe0bd6bd30832
LenaBett <rheabett@gmail.com>
2023-09-04 00:30:12 +0300

    Working with one dimensional arrays Calculator assignment correction

--- src/main/java/com/systechafrica/Assignments/Calculator.java
+++ src/main/java/com/systechafrica/Assignments/Calculator.java
@@ -8,13 +8,13 @@ public class Calculator {
 
     private static final Logger LOGGER = Logger.getLogger(WorkingWithArrays.class.getName());
 
-    public void main() {
+    public static void main(String[] args) {
 
         int[] numberList = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
         for (int i = 0; i < numberList.length; i++) {
-            if (numberList[i] % 2 != 0) {
-                LOGGER.info("" + numberList[i]);
+            if (i % 2 != 0) { // if i is odd tben print the value stored in that index
+                LOGGER.info("Numbers in the odd positions in the array are: " + numberList[i]);
             }
         }
 
