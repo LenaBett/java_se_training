5f3e5f037db0d84a04994979a9680e235845117a
LenaBett <rheabett@gmail.com>
2023-09-04 01:25:57 +0300

    Working with Array and Calculator assignment

--- src/main/java/com/systechafrica/Assignments/Calculator.java
+++ src/main/java/com/systechafrica/Assignments/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
         int[] numberList = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
         for (int i = 0; i < numberList.length; i++) {
-            if (i % 2 != 0) { // if i is odd tben print the value stored in that index
+            if (i % 2 != 0) { // if [i] is odd then print the value stored in that index
                 LOGGER.info("Numbers in the odd positions in the array are: " + numberList[i]);
             }
         }
