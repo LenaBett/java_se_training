c945ac3b9dc69358353863ac22716d4137435bbb
LenaBett <rheabett@gmail.com>
2023-09-24 00:24:58 +0300

    Inheritance

---
+++ src/main/java/com/systechafrica/Part2/Inheritance/CompanySetup/DepartmentHead.java
@@ -0,0 +1,13 @@
+package com.systechafrica.Part2.Inheritance.CompanySetup;
+
+public class DepartmentHead extends Employee {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/systechafrica/Part2/Inheritance/CompanySetup/QualityAssurance.java
@@ -0,0 +1,19 @@
+package com.systechafrica.Part2.Inheritance.CompanySetup;
+
+public class QualityAssurance extends  Employee{
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Name:" + getEmployeeName() + "  Number: " + getEmployeeNo() + "  Address: " + getEmployeeAddress() + "  Title: " + getTitle();
+    }
+
+}
