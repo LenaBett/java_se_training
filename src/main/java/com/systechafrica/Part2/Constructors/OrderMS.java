6f47c5686d7024233f34acd46b49db28a5fd7dc2
LenaBett <rheabett@gmail.com>
2023-09-24 13:02:31 +0300

    Constructors 2 and method overriding

---
+++ src/main/java/com/systechafrica/Part2/Constructors/OrderMS.java
@@ -0,0 +1,7 @@
+package com.systechafrica.Part2.Constructors;
+
+public class OrderMS {
+    public boolean orderBook(Book book, Customer customer){
+        return false;
+    }
+}
--- src/main/java/com/systechafrica/Part2/Polymorphism/PolymorphismDemo.java
+++ src/main/java/com/systechafrica/Part2/Polymorphism/PolymorphismDemo.java
@@ -12,21 +12,24 @@ import com.systechafrica.Part2.Inheritance.ProtectedAccessModifier.Nomencleture.
 public class PolymorphismDemo {
     public static void main(String[] args) {
 
+        // Objects of the QualityAssurance class are treated as objects of the Employee class(the parent class)
         Employee james = new QualityAssurance("001", "James",
                 "047", "Quality Assurance Chief Engineer");
 
+        //Objects of the SoftwareEngineer class are treated as objects of the Employee class(the parent class)
         Employee ivy = new SoftwareEngineer("002", "Ivy",
                 "035", "Senior Software Engineer");
 
-        Employee lenna = new DatabaseEngineer("003", "Lenna",
+        // Objects of the DatabaseEngineer class are treated as objects of the Employee class(the parent class)
+        Employee lena = new DatabaseEngineer("003", "Lena",
                 "035", "Senior Database Engineer");
 
         System.out.println(james);
         System.out.println(ivy);
-        System.out.println(lenna);
+        System.out.println(lena);
 
 
-        Kingdom species = new Species();
+        Kingdom species = new Species(); // objects of species are objects of Kingdom
         Kingdom genus = new Genus();
 
 
